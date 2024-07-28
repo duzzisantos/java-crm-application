407584448e6ee17a747176455d44954176e94e91
Duzie <duzzi4x@gmail.com>
2024-07-28 17:54:36 +0200

    Created new web services for all entities

--- build/generated-sources/ap-source-output/entity/Campaignenrollments_.java
+++ build/generated-sources/ap-source-output/entity/Campaignenrollments_.java
@@ -4,16 +4,19 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2024-07-18T19:42:39")
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2024-07-28T16:58:42")
 @StaticMetamodel(Campaignenrollments.class)
 public class Campaignenrollments_ { 
 
     public static volatile SingularAttribute<Campaignenrollments, Integer> enrollmentId;
-    public static volatile SingularAttribute<Campaignenrollments, Boolean> hasResponded;
     public static volatile SingularAttribute<Campaignenrollments, Integer> productId;
+    public static volatile SingularAttribute<Campaignenrollments, String> responseEmail;
+    public static volatile SingularAttribute<Campaignenrollments, Boolean> hasResponded;
     public static volatile SingularAttribute<Campaignenrollments, String> campaignManager;
     public static volatile SingularAttribute<Campaignenrollments, Integer> campaignId;
+    public static volatile SingularAttribute<Campaignenrollments, Integer> purchaseQuantity;
     public static volatile SingularAttribute<Campaignenrollments, Integer> customerId;
+    public static volatile SingularAttribute<Campaignenrollments, Double> offeredPrice;
     public static volatile SingularAttribute<Campaignenrollments, String> campaignName;
     public static volatile SingularAttribute<Campaignenrollments, Integer> accountManagerId;
     public static volatile SingularAttribute<Campaignenrollments, String> campaignContent;
